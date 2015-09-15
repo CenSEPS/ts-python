b589943c2cd7366f496aeb16a3ce22aa5d1e8c8c
Zachary W. Graham <zwgraham@soe.ucsc.edu>
2015-09-15 14:49:22 -0700

    All FPGA register access now happens in C
    Python mmap cannot reliably read the 16 bit registers. This access is
    critical to userspace hardware control. _syscon has been replaced with
    _mmio which wrapps a few simple C functions to properly access the
    16-bit registers.

---
+++ build_c_extension.py
@@ -0,0 +1,4 @@
+from ts7250v2._mmio import _build_c_extension
+
+if __name__ == "__main__":
+    _build_c_extension()
--- ts7250v2/_mmio.py
+++ ts7250v2/_mmio.py
@@ -1,59 +1,60 @@
 from cffi import FFI
 
 
+def _build_c_extension():
+    ffi = FFI()
+
+    ffi.cdef("""
+    typedef int... off_t;
+    #define PROT_READ ...
+    #define PROT_WRITE ...
+    #define PROT_EXEC ...
+    #define PROT_NONE ...
+    #define MAP_PRIVATE ...
+    #define MAP_FIXED ...
+    #define MAP_SHARED ...
+
+    void mmap_init(off_t offset);
+    uint16_t mmap_peek16(off_t offset);
+    void mmap_poke16(off_t offset, uint16_t value);
+    """)
+
+    ffi.set_source("_cmmapwrapper", """
+    #include<sys/mman.h>
+    #include<sys/types.h>
+    #include<sys/stat.h>
+    #include<fcntl.h>
+    volatile uint16_t *map = 0;
+    void mmap_init(off_t offset){
+        int fd;
+        fd = open("/dev/mem", O_RDWR|O_SYNC);
+        map = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, offset);
+    }
+    uint16_t  mmap_peek16(off_t offset){
+        uint16_t n;
+        n = map[offset/2];
+        return n;
+    }
+    void mmap_poke16(off_t offset, uint16_t value){
+        map[offset/2] = value;
+    }
+    """)
+    ffi.compile(tmpdir='build')
+
+
 class MMIO(object):
     """Class MMIO handles memory-mapped I/O functionality"""
     def __init__(self, offset):
         if (type(offset) != int) and (type(offset) != long):
             raise TypeError("Argument offset must be of type `long` or type `int`")
-
-        ffi = FFI()
-
-        # can this read from a file?
-        ffi.cdef("""
-        typedef long off_t;
-        #define PROT_READ ...
-        #define PROT_WRITE ...
-        #define PROT_EXEC ...
-        #define PROT_NONE ...
-        #define MAP_PRIVATE ...
-        #define MAP_FIXED ...
-        #define MAP_SHARED ...
-
-        void mmap_init(void);
-        uint16_t mmap_peek16(off_t offset);
-        void mmap_poke16(off_t offset, uint16_t value);
-        """)
-
-        # can this be read from a file?
-        # do we need to use set source?
-        self.cmmap = ffi.verify("""
-        #include<sys/mman.h>
-        #include<sys/types.h>
-        #include<sys/stat.h>
-        #include<fcntl.h>
-        volatile uint16_t *map = 0;
-        void mmap_init(void){
-            int fd;
-            fd = open("/dev/mem", O_RDWR|O_SYNC);
-            map = mmap(0, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x80004000);
-        }
-        uint16_t  mmap_peek16(off_t offset){
-            uint16_t n;
-            n = map[offset/2];
-            return n;
-        }
-        void mmap_poke16(off_t offset, uint16_t value){
-            map[offset/2] = value;
-        }
-        """)
-
-        self.cmmap.mmap_init()
+        from _cmmapwrapper import lib
+        self.cmmap = lib
+        self.cmmap.mmap_init(offset)
 
     def peek16(self, offset):
         # bounds check on offset being withing the range defined by TS
 
-        #call ffi peek16 method
+        # call ffi peek16 method
         return self.cmmap.mmap_peek16(offset)
 
     def poke16(self, offset, value):
@@ -61,5 +62,5 @@ class MMIO(object):
 
         # bounds check on limits for value
 
-        #call ffi poke16 method
+        # call ffi poke16 method
         self.cmmap.mmap_poke16(offset, value)
